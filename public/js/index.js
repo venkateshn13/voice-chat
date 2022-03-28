404f17e5898679afdee752fc92a050b4aea783da
Venkatesh.N <venkatesh.n@attinadsoftware.com>
2022-03-28 14:33:03 +0530

    changed https

--- public/js/index.js
+++ public/js/index.js
@@ -18,7 +18,7 @@ const userStatus = {
     mainFunction(1000);
   };
   
-  var socket = io("ws://https://voice-chat-web.herokuapp.com");
+  var socket = io("ws://voice-chat-web.herokuapp.com");
   socket.emit("userInformation", userStatus);
   
   
