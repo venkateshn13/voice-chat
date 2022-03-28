ba9b1ac0657248f19219bab60c37176df4144e26
Venkatesh.N <venkatesh.n@attinadsoftware.com>
2022-03-28 14:16:01 +0530

    changed ws url

--- public/js/index.js
+++ public/js/index.js
@@ -18,7 +18,7 @@ const userStatus = {
     mainFunction(1000);
   };
   
-  var socket = io("ws://localhost:3000");
+  var socket = io("ws://https://voice-chat-web.herokuapp.com");
   socket.emit("userInformation", userStatus);
   
   
